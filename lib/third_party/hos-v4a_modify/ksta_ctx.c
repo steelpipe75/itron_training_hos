d6a377c06eda0434afe3069756c9be3a5ac4adfd
Nishizaki <steelpipe75@gmail.com>
2023-10-24 22:08:08 +0900

    sampleアプリケーションのコマンドライン表示のみ版（MessageBoxなし版）を追加

--- lib/third_party/hos-v4a_modify/ksta_ctx.c
+++ lib/third_party/hos-v4a_modify/ksta_ctx.c
@@ -1,3 +1,9 @@
+#if 1
+/**
+ * hos-v4a の kernel/source/arch/proc/win/win32/ksta_ctx.c
+ * をもとに改変
+ */
+#endif
 /** 
  *  Hyper Operating System V4 Advance
  *
@@ -10,7 +16,9 @@
 
 
 #include "core/core.h"
-
+#if 1
+#include <stdio.h>
+#endif
 
 /** %jp{コンテキストの開始} */
 void _kernel_sta_ctx(_KERNEL_T_CTXCB *ctxcb)
@@ -23,10 +31,7 @@ void _kernel_sta_ctx(_KERNEL_T_CTXCB *ctxcb)
 	
 	if (GetCurrentThreadId() == _kernel_ictxcb.dwPrimaryThreadId)
 	{
-#if 0
-		/* %jp{ダイアログを表示} */
-		MessageBox(NULL, _T("Press OK, Exit a process"), _T("Hyper Operationg System V4 Advance for Win32"), MB_OK);
-#else
+#if 1
 		printf("#### Hit Space Key to Exit ####\n");
 		while(1) {
 			if(GetAsyncKeyState(VK_SPACE) & 0x01 ) {
@@ -34,6 +39,9 @@ void _kernel_sta_ctx(_KERNEL_T_CTXCB *ctxcb)
 			}
 			Sleep(1);
 		}
+#else
+		/* %jp{ダイアログを表示} */
+		MessageBox(NULL, _T("Press OK, Exit a process"), _T("Hyper Operationg System V4 Advance for Win32"), MB_OK);
 #endif
 
 		/* %jp{終了} */

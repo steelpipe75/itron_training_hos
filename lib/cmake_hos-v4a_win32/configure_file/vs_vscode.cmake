048eda92a1dfb957b37f8af27df79e7beffe764d
Nishizaki <steelpipe75@gmail.com>
2023-10-27 22:01:23 +0900

    .vscodeフォルダの位置指定間違い修正

--- lib/cmake_hos-v4a_win32/configure_file/vs_vscode.cmake
+++ lib/cmake_hos-v4a_win32/configure_file/vs_vscode.cmake
@@ -16,8 +16,8 @@ macro(vs_and_vscode_helper ARG_EXE_FILE_NAME)
     set(LAUNCH_JSON_EXE_FILE ${ARG_EXE_FILE_NAME}.exe)
 
     # Visual Studio Code用 設定ファイル
-    configure_file(${WORKSPACE_ROOT_DIR}/lib/cmake_hos-v4a_win32/configure_file/tasks.json.in  ${WORKSPACE_ROOT_DIR}/../.vscode/tasks.json)
+    configure_file(${WORKSPACE_ROOT_DIR}/lib/cmake_hos-v4a_win32/configure_file/tasks.json.in  ${WORKSPACE_ROOT_DIR}/.vscode/tasks.json)
     if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        configure_file(${WORKSPACE_ROOT_DIR}/lib/cmake_hos-v4a_win32/configure_file/launch.json.in ${WORKSPACE_ROOT_DIR}/../.vscode/launch.json)
+        configure_file(${WORKSPACE_ROOT_DIR}/lib/cmake_hos-v4a_win32/configure_file/launch.json.in ${WORKSPACE_ROOT_DIR}/.vscode/launch.json)
     endif()
 endmacro()

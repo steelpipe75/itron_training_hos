6bdf270a8b58075d204e288648760d0a53966b01
Nishizaki <steelpipe75@gmail.com>
2023-11-11 23:38:16 +0900

    Update ksta_ctx.c

--- lib/third_party/hos-v4a_modify/extension/src/ksta_ctx.c
+++ lib/third_party/hos-v4a_modify/extension/src/ksta_ctx.c
@@ -50,10 +50,10 @@ void _kernel_sta_ctx(_KERNEL_T_CTXCB *ctxcb)
 #else
 		/* %jp{ダイアログを表示} */
 		MessageBox(NULL, _T("Press OK, Exit a process"), _T("Hyper Operationg System V4 Advance for Win32"), MB_OK);
-#endif
 		
 		/* %jp{終了} */
 		ExitProcess(0);
+#endif
 	}
 }
 

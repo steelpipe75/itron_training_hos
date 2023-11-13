a608dfd82eae2a5f54919fdaa5be808d5a1edb6c
Nishizaki <steelpipe75@gmail.com>
2023-11-13 22:06:34 +0900

    Update compile_opion_helper.cmake

--- lib/cmake_hos-v4a_win32/compile_option/compile_opion_helper.cmake
+++ lib/cmake_hos-v4a_win32/compile_option/compile_opion_helper.cmake
@@ -1,4 +1,6 @@
 
+option(SW_OPT_no_unused_parameter OFF "if \"ON\" add compile option \"-Wno-unused-parameter\"")
+
 macro(target_compile_options_helper ARG_TARGET_NAME)
     target_compile_options(${ARG_TARGET_NAME}
         PRIVATE $<$<C_COMPILER_ID:MSVC>:/W4 /WX- /utf-8>
@@ -11,4 +13,10 @@ macro(target_compile_options_helper ARG_TARGET_NAME)
         PRIVATE $<$<AND:$<CXX_COMPILER_ID:Clang>,$<CONFIG:Debug>>:-g -O0>
         PRIVATE $<$<AND:$<CXX_COMPILER_ID:Clang>,$<CONFIG:Release>>:-O3 -DNDEBUG>
     )
+    if(${SW_OPT_no_unused_parameter})
+        target_compile_options(${ARG_TARGET_NAME}
+            PRIVATE $<$<AND:$<C_COMPILER_ID:Clang>>:-Wno-unused-parameter>
+            PRIVATE $<$<AND:$<CXX_COMPILER_ID:Clang>>:-Wno-unused-parameter>
+        )
+    endif()
 endmacro()

b5f52c1c1caa559a8fa55d48c3b731ea47b18140
Nishizaki <steelpipe75@gmail.com>
2023-11-20 22:16:43 +0900

    Update clean.bat

--- dev/clean.bat
+++ dev/clean.bat
@@ -1,6 +1,10 @@
 cd /d %~dp0
 
+RMDIR /S /Q .\extension\aplfw\build
+RMDIR /S /Q .\extension\sample\build
+RMDIR /S /Q .\extension\sample_cli\build
+
 RMDIR /S /Q .\googletest\build
-RMDIR /S /Q .\vter_knl\build
+RMDIR /S /Q .\googlemock\build
 
 cmd /k

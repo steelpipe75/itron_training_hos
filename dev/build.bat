f6eb2b7b87c9919eb5679c670491f5ea96cc47d4
Nishizaki <steelpipe75@gmail.com>
2023-11-20 22:32:44 +0900

    Update build.bat

--- dev/build.bat
+++ dev/build.bat
@@ -15,6 +15,11 @@ cmake --build ./extension/sample_cli/build/Ninja
 cmake -B      ./extension/sample_cli/build/VS2017   -S ./extension/sample_cli   -G "Visual Studio 15 2017"
 cmake --build ./extension/sample_cli/build/VS2017
 
+cmake -B      ./extension/aplfw_cli/build/Ninja     -S ./extension/aplfw_cli    -G "Ninja"
+cmake --build ./extension/aplfw_cli/build/Ninja
+cmake -B      ./extension/aplfw_cli/build/VS2017    -S ./extension/aplfw_cli    -G "Visual Studio 15 2017"
+cmake --build ./extension/aplfw_cli/build/VS2017
+
 cmake -B      ./googletest/build/Ninja              -S ./googletest             -G "Ninja"
 cmake --build ./googletest/build/Ninja
 cmake -B      ./googletest/build/VS2017             -S ./googletest             -G "Visual Studio 15 2017"

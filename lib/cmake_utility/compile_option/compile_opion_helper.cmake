b1b920f4fffe2c28fa008deabae0307d6c4ce6d5
Nishizaki <steelpipe75@gmail.com>
2023-12-28 08:10:02 +0900

    カバレッジ出力対応

--- lib/cmake_utility/compile_option/compile_opion_helper.cmake
+++ lib/cmake_utility/compile_option/compile_opion_helper.cmake
@@ -7,11 +7,13 @@ macro(target_compile_options_helper ARG_TARGET_NAME)
         PRIVATE $<$<C_COMPILER_ID:Clang>:-Wall -Wextra>
         PRIVATE $<$<AND:$<C_COMPILER_ID:Clang>,$<CONFIG:Debug>>:-g -O0>
         PRIVATE $<$<AND:$<C_COMPILER_ID:Clang>,$<CONFIG:Release>>:-O3 -DNDEBUG>
+        PRIVATE $<$<AND:$<C_COMPILER_ID:Clang>,$<CONFIG:Coverage>>:-g -O0 -fprofile-instr-generate -fcoverage-mapping>
 
         PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/W4 /WX- /utf-8>
         PRIVATE $<$<CXX_COMPILER_ID:Clang>:-Wall -Wextra>
         PRIVATE $<$<AND:$<CXX_COMPILER_ID:Clang>,$<CONFIG:Debug>>:-g -O0>
         PRIVATE $<$<AND:$<CXX_COMPILER_ID:Clang>,$<CONFIG:Release>>:-O3 -DNDEBUG>
+        PRIVATE $<$<AND:$<CXX_COMPILER_ID:Clang>,$<CONFIG:Coverage>>:-g -O0 -fprofile-instr-generate -fcoverage-mapping>
     )
     if(${SW_OPT_no_unused_parameter})
         target_compile_options(${ARG_TARGET_NAME}
